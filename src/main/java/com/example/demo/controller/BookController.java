1ad115cb013f3c8718333f39654e706b8027b9d8
Dmabl <sdfghj@dg.jnb>
2023-01-14 22:13:30 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,28 @@
+package com.example.api_swagger.controller;
+
+import com.example.api_swagger.model.Book;
+import com.example.api_swagger.services.BookService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("books")
+public class BookController {
+
+
+    private BookService bookService;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("{id}")  // GET http://localhost:8080/books/23
+    public Book getBookInfo(@PathVariable long id) {
+
+        return bookService.findBook(id);
+    }
+
+    @PostMapping() // POST http://localhost:8080/books/23
+    public Book createBook (Book book) {
+        return bookService.createBook(book);
+    }
+
+}
---
+++ src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,59 @@
+package com.example.api_swagger.model;
+
+import java.util.Objects;
+
+public class Book {
+
+    private long id;
+    private String name;
+    private String author;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, author);
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
