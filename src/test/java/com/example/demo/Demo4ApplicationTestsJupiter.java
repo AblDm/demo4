88a056428d271c8b7c6619fc0305d75b0dd18a31
Dmabl <sdfghj@dg.jnb>
2023-02-05 18:04:59 +0100

    Tests

--- src/test/java/com/example/demo/Demo4ApplicationTestsJupiter.java
+++ src/test/java/com/example/demo/Demo4ApplicationTestsJupiter.java
@@ -57,54 +57,10 @@ class Demo4ApplicationTestsJupiter {
         book.setId(id);
 
         Assertions
-                .assertThat(this.restTemplate.postForObject("http://localhost:"+ port +"/books", book, String.class))
+                .assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/books", book, String.class))
                 .isNotNull();
+
     }
-
-
-//    @Test
-//    public void testSaveBookTest() throws Exception {
-//
-//        final String name = "testBook";
-//        final String author = "testAuthor";
-//        final long id = 1;
-//
-//
-//        JSONObject bookObject = new JSONObject();
-//
-//        bookObject.put("name",name);
-//        bookObject.put("author",author);
-//
-//        Book book = new Book();
-//        book.setId(id);
-//        book.setName(name);
-//        book.setAuthor(author);
-//
-//        when(bookRepository.save(any(Book.class))).thenReturn(book);
-//        when(bookRepository.findById(any(Long.class))).thenReturn(Optional.of(book));
-//
-//        mockMvc.perform(MockMvcRequestBuilders
-//                .post("/books/" )
-//                .content(bookObject.toString())
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").value(id))
-//                .andExpect(jsonPath("$.name").value(name))
-//                .andExpect(jsonPath("$.author").value(author));
-//        mockMvc.perform(MockMvcRequestBuilders
-//                .get("/books/" + id)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").value(id))
-//                .andExpect(jsonPath("$.name").value(name))
-//                .andExpect(jsonPath("$.author").value(author));
-//    }
-//
-//
-//
-
-
 
 
 
